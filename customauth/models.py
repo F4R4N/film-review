abb6be1990b9cd71d906dea34cc35ce8dde958be
faran <farantgh@gmail.com>
2021-03-22 23:02:31 -0400

    added related name to user field

--- customauth/models.py
+++ customauth/models.py
@@ -5,9 +5,10 @@ from api.utils import profile_image, random_key
 
 class Profile(models.Model):
 	key = models.CharField(max_length=16, default=random_key, unique=True, blank=False, null=False)
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 	image = models.ImageField(upload_to=profile_image, default="profile/default/default.png")
 	group = models.ManyToManyField(Group)
+	admin_of_group = models.CharField(default=False, max_length=50)
 
 	def __str__(self):
 		return self.user.username
