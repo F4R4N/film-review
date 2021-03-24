566015022a2ad08ce47e29dde4c8594b72f42773
faran <farantgh@gmail.com>
2021-03-23 20:29:57 -0400

    added new features url path

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
-from .views import CreateGroup
+from .views import CreateGroupView, CreateMovieView, EditMovieView, GetRandomMovieView, SubmitMovieView
 from django.urls import path
 
 app_name = "api"
 urlpatterns = [
-	path("group/add/", CreateGroup.as_view()),
+	path("group/add/", CreateGroupView.as_view()),
+	path("movie/add/", CreateMovieView.as_view()),
+	path("movie/edit/<str:key>/", EditMovieView.as_view()),
+	path("movie/group/select/<str:key>/", GetRandomMovieView.as_view()),
+	path("movie/group/submit/<str:group>/<str:movie>/", SubmitMovieView.as_view()),
 ]
