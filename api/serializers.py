6517e21d5a2e307aa0604c21dea7c6ac1df8b7c2
faran <farantgh@gmail.com>
2021-03-25 00:04:42 -0400

    added GroupMemberSerializer MovieProfileSerializer MemberSerializer serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
 from .models import Group, Movie
 from django.contrib.auth.models import User
-
+from customauth.models import Profile
 
 class AdminSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ("username", )
-
 
 class MovieSerializer(serializers.ModelSerializer):
 	user = AdminSerializer(read_only=True, many=False)
@@ -16,6 +15,20 @@ class MovieSerializer(serializers.ModelSerializer):
 		model = Movie
 		fields = ("key", "name", "description", "user", "year", "imdb_rate", "watched", "download_link", "poster_link")
 
+class MovieProfileSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Movie
+		fields = ("key", "name", "description", "year", "imdb_rate", "watched", "download_link", "poster_link")
+
+
+class MemberSerializer(serializers.ModelSerializer):
+	movies = MovieProfileSerializer(many=True, source='movie')
+	
+	class Meta:
+		model = User
+		fields = ("username", "movies")
+
 
 class GroupSerializer(serializers.ModelSerializer):
 	admin = AdminSerializer(read_only=True, many=False)
@@ -23,3 +36,9 @@ class GroupSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Group
 		fields = ("key", "name", "movie_of_the_week", "admin", "invite_link")
+
+class GroupMemberSerializer(serializers.ModelSerializer):
+	user = MemberSerializer(read_only=True)
+	class Meta:
+		model = Profile
+		fields = ("key", "image", "user")
