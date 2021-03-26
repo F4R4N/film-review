38865bfebf73d89f0b91cc64824c7a3601f4804c
faran <farantgh@gmail.com>
2021-03-26 19:07:04 -0400

    added a custom admi panel configuration for User model and list_display for Orifile model

--- customauth/admin.py
+++ customauth/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Profile
+from django.contrib.auth.models import User
 
-admin.site.register(Profile)
+# admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ("user", "image", "key", )
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ("username", "first_name", "last_name", "email", "is_active", "is_staff", )
+	list_editable = ("email", "is_active",)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
