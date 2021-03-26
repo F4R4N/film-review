0ff4e71e12c805044fa0a7cbc745c355469787d3
faran <farantgh@gmail.com>
2021-03-25 21:38:24 -0400

    added a function for group image file handling

--- api/utils.py
+++ api/utils.py
@@ -5,6 +5,10 @@ def profile_image(instance, filename):
     saved_file_name = instance.user.username + "-" + datetime.now().strftime("%Y_%m_%d,%H:%M:%S") + ".jpg"
     return 'profile/{0}/{1}'.format(instance.user.username, saved_file_name)
 
+def group_image(instance, filename):
+    saved_file_name = instance.name + "-" + datetime.now().strftime("%Y_%m_%d,%H:%M:%S") + ".jpg"
+    return 'group/{0}/{1}'.format(instance.name, saved_file_name)
+
 def random_key():
 	allowed_chars = list(string.ascii_lowercase) + list(string.digits)
 	random_key = ""
