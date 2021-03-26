0565fbadd3a3658c1102ad870fe6f830d3bbd789
faran <farantgh@gmail.com>
2021-03-25 21:00:38 -0400

    add review field for movie object and removed OMDBMovie field

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .utils import random_key, invite_url
+from .utils import random_key, invite_code
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 import datetime
@@ -8,6 +8,7 @@ class Movie(models.Model):
 	key = models.CharField(max_length=15, default=random_key, unique=True)
 	name = models.CharField(max_length=100)
 	description = models.TextField(null=True, blank=True)
+	review = models.TextField(null=True, blank=True)
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="movie")
 	year = models.PositiveIntegerField(validators=[MinValueValidator(1800), MaxValueValidator(datetime.datetime.now().year)], null=True, blank=True)
 	imdb_rate = models.FloatField(validators=[MinValueValidator(1), MaxValueValidator(10)], null=True, blank=True)
@@ -25,12 +26,8 @@ class Group(models.Model):
 	name = models.CharField(max_length=50)
 	movie_of_the_week = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
 	admin = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-	invite_link = models.CharField(max_length=15, default=invite_url)
+	invite_code = models.CharField(max_length=17, default=invite_code)
 	date_and_time = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.name
-
-
-class OMDBMovie(models.Model):
-	json_data = models.JSONField()
