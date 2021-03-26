42ee87f8150c6dc22b314b8767d40f544055d328
faran <farantgh@gmail.com>
2021-03-25 21:02:40 -0400

    added new features url path

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,16 @@
-from .views import CreateGroupView, CreateMovieView, EditMovieView, GetRandomMovieView, SubmitMovieView, AllUserGroups, AllGroupMembersProfile
+from .views import CreateGroupView, EditAndDeleteGroupView, EditAndDeleteMovieView, CreateAndGetMovieView, GetRandomMovieView, SubmitMovieView, AllUserGroups, AllGroupMembersProfile, GenerateInviteCode, JoinGroup
 from django.urls import path
 
 app_name = "api"
 urlpatterns = [
 	path("group/add/", CreateGroupView.as_view()),
-	path("movie/add/", CreateMovieView.as_view()),
-	path("movie/edit/<str:key>/", EditMovieView.as_view()),
-	path("movie/group/select/<str:key>/", GetRandomMovieView.as_view()),
-	path("movie/group/submit/<str:group>/<str:movie>/", SubmitMovieView.as_view()),
-	path("group/get/", AllUserGroups.as_view()),
+	path("group/<str:group_key>/", EditAndDeleteGroupView.as_view()),
+	path("admin/group/invite_code/<str:group_key>/", GenerateInviteCode.as_view()),
+	path("group/join/<str:invite_code>/", JoinGroup.as_view()),
+	path("group/movie/select/<str:key>/", GetRandomMovieView.as_view()),
+	path("group/movie/submit/<str:group>/<str:movie>/", SubmitMovieView.as_view()),
 	path("group/all_profiles/<str:group_key>/", AllGroupMembersProfile.as_view()),
+	path("user/groups/", AllUserGroups.as_view()),
+	path("movie/", CreateAndGetMovieView.as_view()),
+	path("movie/<str:key>/", EditAndDeleteMovieView.as_view()),
 ]
