1b39683e4431f25697da78d89a22919cd9033a66
faran <farantgh@gmail.com>
2021-03-25 21:03:19 -0400

    renamed invite_url function to invite_code

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,6 @@
 import random, string
 from datetime import datetime
+
 def profile_image(instance, filename):
     saved_file_name = instance.user.username + "-" + datetime.now().strftime("%Y_%m_%d,%H:%M:%S") + ".jpg"
     return 'profile/{0}/{1}'.format(instance.user.username, saved_file_name)
@@ -10,7 +11,7 @@ def random_key():
 	key = random_key.join(random.sample(allowed_chars, 15))
 	return key
 
-def invite_url():
+def invite_code():
 	allowed_chars = list(string.ascii_lowercase)
 	random_key = ""
 	key = random_key.join(random.sample(allowed_chars, 15))
