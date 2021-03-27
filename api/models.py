7efa71e38dcc9c3e88aeb9979d381ea5c80be5ff
faran <farantgh@gmail.com>
2021-03-26 22:38:32 -0400

    added defaukt image for group

--- api/models.py
+++ api/models.py
@@ -24,7 +24,7 @@ class Movie(models.Model):
 class Group(models.Model):
 	key = models.CharField(max_length=15, default=random_key)
 	name = models.CharField(max_length=50)
-	image = models.ImageField(upload_to=group_image, default=None)
+	image = models.ImageField(upload_to=group_image, default="group/default/default.png")
 	movie_of_the_week = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, blank=True)
 	admin = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 	invite_code = models.CharField(max_length=17, default=invite_code)
