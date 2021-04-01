2c3714dc88c88dcbf9d339fd2438ed3985935e74
faran <farantgh@gmail.com>
2021-04-01 15:53:39 -0400

    added leave group url path for group

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
-from .views import CreateGroupView, EditAndDeleteGroupView, EditAndDeleteMovieView, CreateAndGetMovieView, GetRandomMovieView, SubmitMovieView, AllUserGroups, AllGroupMembersProfile, GenerateInviteCode, JoinGroup
+from .views import (CreateGroupView, EditAndDeleteGroupView, EditAndDeleteMovieView, 
+					CreateAndGetMovieView, GetRandomMovieView, SubmitMovieView, 
+					AllUserGroups, AllGroupMembersProfile, GenerateInviteCode, JoinGroup, LeaveGroup)
 from django.urls import path
 
 app_name = "api"
@@ -7,6 +9,7 @@ urlpatterns = [
 	path("group/<str:group_key>/", EditAndDeleteGroupView.as_view()),
 	path("admin/group/invite_code/<str:group_key>/", GenerateInviteCode.as_view()),
 	path("group/join/<str:invite_code>/", JoinGroup.as_view()),
+	path("group/leave/<str:group_key>/", LeaveGroup.as_view()),
 	path("group/movie/select/<str:key>/", GetRandomMovieView.as_view()),
 	path("group/movie/submit/<str:group>/<str:movie>/", SubmitMovieView.as_view()),
 	path("group/all_profiles/<str:group_key>/", AllGroupMembersProfile.as_view()),
