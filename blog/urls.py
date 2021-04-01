51d8e450a3165cda008a488458732ab12b8a0615
faran <farantgh@gmail.com>
2021-04-01 15:59:28 -0400

    added show all posts, show group posts, and full detail posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import CreateAndGetUserPost, EditAndDeletePost
+from .views import CreateAndGetUserPost, EditAndDeletePost, AllPublicPostsPaginated, DesiredPost, GroupPublicPostsPaginated
 app_name = "blog"
 
 urlpatterns = [
 	path("post/", CreateAndGetUserPost.as_view()),
 	path("post/<str:key>/", EditAndDeletePost.as_view()),
+	path("posts/all/", AllPublicPostsPaginated.as_view()),
+	path("posts/<str:key>/", DesiredPost.as_view()),
+	path("posts/group/<str:group_key>/", GroupPublicPostsPaginated.as_view()),
+
 
 ]
