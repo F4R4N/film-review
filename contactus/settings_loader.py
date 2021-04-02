a4cff41199cf3ed7161e996af663b8e1eaa40027
faran <farantgh@gmail.com>
2021-04-02 19:57:29 -0400

    added contactus app

---
+++ contactus/settings_loader.py
@@ -0,0 +1,26 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+DEFAULT_CONTACT_US_SETTINGS = {
+	'APP_NAME': None,
+	'SEND_MAIL': False,
+	'MAIL_SUBJECT': " Contact Us ",
+	'MESSAGE': "\nwe got your email. we will respond as soon as possible.\n\nBest Regards, "
+}
+
+try:
+	from django.conf import settings
+except ImportError:
+	print("add CONTACT_US_SETTINGS to projects settings.py")
+	exit()
+fields = ["APP_NAME", "SEND_MAIL", "MAIL_SUBJECT", "MESSAGE"]
+
+for field in fields:
+	if field in settings.CONTACT_US_SETTINGS:
+		DEFAULT_CONTACT_US_SETTINGS[field] = settings.CONTACT_US_SETTINGS[field]
+
+try:
+	from django.conf import settings
+except ImportError:
+	raise ValidationError({"detail": "please add email backend in projects settings.py", 
+								"data": ["EMAIL_USE_TLS", "EMAIL_HOST", "EMAIL_PORT", "EMAIL_HOST_USER", "EMAIL_HOST_PASSWORD"]})
---
+++ contactus/views.py
@@ -0,0 +1,88 @@
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from .models import Contact, MassEmail
+from .utils import get_client_ip
+from .serializers import ContactSerializer
+from . import settings_loader
+
+
+class CreateForm(APIView):
+	permission_classes = (permissions.AllowAny, )
+
+	def post(self, request, format=None):
+		required_fields = ["name", "email", "text"]
+		for field in required_fields:
+			if not field in request.data:
+				return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "{} is required".format(field)})
+		ip = get_client_ip(request)
+		contact = Contact.objects.create(
+			name=request.data["name"],
+			email=request.data["email"],
+			text=request.data["text"]
+		)
+		contact.ip = ip
+		if "subject" in request.data:
+			contact.subject = request.data["subject"]
+		if "phone_number" in request.data:
+			contact.phone_number = request.data["phone_number"]
+		if "address" in request.data:
+			contact.address = request.data["address"]
+
+		contact.save()
+		if settings_loader.DEFAULT_CONTACT_US_SETTINGS["SEND_MAIL"]:
+			admin_name = list(User.objects.filter(is_superuser=True, is_staff=True).values_list("first_name", flat=True))[0]
+			print(admin_name)
+			mail_subject = settings_loader.DEFAULT_CONTACT_US_SETTINGS["APP_NAME"] + settings_loader.DEFAULT_CONTACT_US_SETTINGS["MAIL_SUBJECT"]
+			
+			message = "dear, " + contact.name + settings_loader.DEFAULT_CONTACT_US_SETTINGS["MESSAGE"] + admin_name
+			to_email = contact.email
+			send_email = EmailMessage(mail_subject, message, to=[to_email]).send()
+
+		return Response(status=status.HTTP_200_OK, data={"detail": "form created."})
+
+
+class AdminContactReader(APIView):
+	permission_classes = (permissions.IsAdminUser, )
+
+	def get(self, request, year=None, month=None, format=None):
+		contacts = Contact.objects.all()
+		if all([year, month]):
+			contacts = Contact.objects.filter(datetime__year=year, datetime__month=month)
+		serializer = ContactSerializer(instance=contacts, many=True)
+		return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+class AdminEditIsReaded(APIView):
+	permission_classes = (permissions.IsAdminUser, )
+
+	def put(self, request, key, format=None):
+		contact = get_object_or_404(Contact, key=key)
+		if not "is_readed" in request.data:
+			return Response(status=status>HTTP_400_BAD_REQUEST, data={"detail": "'is_readed' field not provided."})
+		contact.is_readed = request.data["is_readed"]
+		contact.save()
+		return Response(status=status.HTTP_200_OK, data={"detail": "updated"})
+
+class AdminSendMassEmail(APIView):
+	permission_classes = (permissions.IsAdminUser, )
+
+	def post(self, request, format=None):
+		fields = ["subject", "text"]
+		for field in fields:
+			if not field in request:
+				return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "field '{0}' not provided.".format(field)})
+
+		email = MassEmail.objects.create(subject=request.data["subject"], text=request.data["text"])
+		if "name" in request.data:
+			email.name = request.data["name"]
+		email.save()
+		mail_subject = request.data["subject"]
+		message = request.data["text"]
+		all_emails = list(Contact.objects.all().values_list("email", flat=True))
+		send_email = EmailMessage(mail_subject, message, to=all_emails).send()
+		return Response(status=status.HTTP_200_OK, data={"detail": "mass email sent.", "emails": all_emails})
+
