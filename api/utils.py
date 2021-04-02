29ec54e2a63eeee0e75bcf6f07b0971632ce44de
faran <farantgh@gmail.com>
2021-04-02 19:47:13 -0400

    moved all_movies_in_group, have_permission_for_group, is_admin_user from view to utils

--- api/utils.py
+++ api/utils.py
@@ -20,3 +20,27 @@ def invite_code():
 	random_key = ""
 	key = random_key.join(random.sample(allowed_chars, 15))
 	return "FILMMEETING" + key
+
+
+def all_movies_in_group(key):
+	group = get_object_or_404(Group, key=key)
+	all_profiles = group.profile_set.all()
+	all_group_movies = []
+	for profile in all_profiles:
+		user_movies = profile.user.movie.filter(watched=False).values_list("key", flat=True)
+		all_group_movies.append(list(user_movies))
+	all_group_movie_keys = list(chain.from_iterable(all_group_movies))
+	return all_group_movie_keys
+
+def have_permission_for_group(group_key, user):
+	if not user.profile.group.filter(key=group_key).exists():
+		return False
+	return True
+
+
+def is_admin_user(group_key, user):
+	group = get_object_or_404(Group, key=group_key)
+	if group.admin != user:
+		return False
+	return True
+
