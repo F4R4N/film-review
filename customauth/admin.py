cc6fa449b36c266bf1a7ed1f1ae45592accb81c5
faran <farantgh@gmail.com>
2021-04-02 19:49:57 -0400

    added comments

--- customauth/admin.py
+++ customauth/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from .models import Profile
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-# admin.site.register(Profile)
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
 	list_display = ("user", "image", "key", )
 
+# changed list_display and list_editable but keep default settings by inheriting UserAdmin
 class MyUserAdmin(UserAdmin):
 	list_display = ("username", "first_name", "last_name", "email", "is_active", "is_staff", )
 	list_editable = ("email", "is_active",)
 
+# to save changes should unregister and re-register the model in Admin Panel
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
