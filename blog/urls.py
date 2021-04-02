bfa35412c2f7084ea771e2267dca858fa493941e
faran <farantgh@gmail.com>
2021-04-02 15:26:16 -0400

    changed the name of passing parameters to make them more specific and readable

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import CreateAndGetUserPost, EditAndDeletePost, AllPublicPostsPaginated, DesiredPost, GroupPublicPostsPaginated
+from .views import (CreateAndGetUserPost, EditAndDeletePost, AllPublicPostsPaginated,
+					DesiredPost, GroupPublicPostsPaginated, CreateComment, EditAndDeleteComment)
+
 app_name = "blog"
 
 urlpatterns = [
 	path("post/", CreateAndGetUserPost.as_view()),
-	path("post/<str:key>/", EditAndDeletePost.as_view()),
+	path("post/<str:post_key>/", EditAndDeletePost.as_view()),
 	path("posts/all/", AllPublicPostsPaginated.as_view()),
-	path("posts/<str:key>/", DesiredPost.as_view()),
+	path("posts/<str:post_key>/", DesiredPost.as_view()),
 	path("posts/group/<str:group_key>/", GroupPublicPostsPaginated.as_view()),
-
+	path("comment/create/<str:post_key>/", CreateComment.as_view()),
+	path("comment/<str:comment_key>/", EditAndDeleteComment.as_view()),
 
 ]
