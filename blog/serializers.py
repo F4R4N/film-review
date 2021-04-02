e21066a9a8742bead65ca3f88881594d40d30ada
faran <farantgh@gmail.com>
2021-04-02 19:48:45 -0400

    added documentation

--- blog/serializers.py
+++ blog/serializers.py
@@ -8,12 +8,19 @@ class TagSerializer(serializers.ModelSerializer):
 		fields = ("name", "slug")
 
 class AuthorSerializer(serializers.ModelSerializer):
+	"""
+		needed data from ahtor to send in PostSerializer and CommentSerializer is only profile.key and username
+	"""
 	key = serializers.CharField(source='profile.key')
 	class Meta:
 		model = User
 		fields = ("key", "username")
 
 class DemoPostSerializer(serializers.ModelSerializer):
+	"""
+		to ensure visits are not fake just by loading the data in frontend. first send demodata that is small part of data .
+		then user can access the full data of post with entering the given link and there we can add  visit
+	"""
 	author = AuthorSerializer()
 	class Meta:
 		model = Post
@@ -26,6 +33,11 @@ class CommentSerializer(serializers.ModelSerializer):
 		fields = ("key", "author", "body", "created")
 
 class PostSerializer(serializers.ModelSerializer):
+	"""
+		full data of a post that if the user click on link of demodata will see.
+		tags, author of post and comments are relations in database so we use their serializers to access the related data.
+		'comments' are backward relational queryset . original should be comment_set but renamed to comments in models.comment
+	"""
 	tags = TagSerializer(many=True)
 	author = AuthorSerializer()
 	comments = CommentSerializer(many=True)
