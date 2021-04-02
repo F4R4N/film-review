daad23a4936de29a13c003e892002a8f32ce2c6f
faran <farantgh@gmail.com>
2021-04-02 15:29:19 -0400

    edited default User admin panel register to inherit the default properties and customize the 'list_display' and 'list_editable' properties

--- customauth/admin.py
+++ customauth/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import Profile
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-
 # admin.site.register(Profile)
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
 	list_display = ("user", "image", "key", )
 
-class UserAdmin(admin.ModelAdmin):
+class MyUserAdmin(UserAdmin):
 	list_display = ("username", "first_name", "last_name", "email", "is_active", "is_staff", )
 	list_editable = ("email", "is_active",)
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, MyUserAdmin)
