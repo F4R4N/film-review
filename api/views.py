bb416469c01b053dda269341306c822305a4ca54
faran <farantgh@gmail.com>
2021-04-08 18:52:57 +0430

    added documentations for endpoints and cleaned the file and make it readable based on flake8

--- api/views.py
+++ api/views.py
@@ -1,15 +1,17 @@
-from rest_framework import permissions, status, generics
+from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from .serializers import GroupSerializer, MovieSerializer, GroupMemberSerializer
+from .serializers import (
+	GroupSerializer, MovieSerializer, GroupMemberSerializer
+	)
 from .models import Group, Movie
-from .utils import invite_code, all_movies_in_group, have_permission_for_group, is_admin_user
+from .utils import (
+	invite_code, all_movies_in_group, have_permission_for_group, is_admin_user
+	)
 from customauth.models import Profile
 from config.settings import MOVIE_PER_USER
 import random
-from itertools import chain 
 
 
 class CreateGroupView(APIView):
@@ -34,16 +36,22 @@ class CreateGroupView(APIView):
 
 			Required
 			request.data["name"] -> String
-			
+
 			Optional
 			request.data["image"] -> Image
 
 		"""
 
-		if not "name" in request.data:
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "name is not in request."})
+		if "name" not in request.data:
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "name is not in request."})
+
 		if Group.objects.filter(name=request.data['name']).exists():
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "group name already exisit."})
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "group name already exisit."})
+
 		group = Group.objects.create(
 			name=request.data['name'],
 			admin=request.user
@@ -54,7 +62,10 @@ class CreateGroupView(APIView):
 		request.user.profile.group.add(group)
 
 		serializer = GroupSerializer(instance=group)
-		return Response(status=status.HTTP_201_CREATED, data={"detail": "group '{0}' added.".format(group.name), "data": serializer.data})
+		return Response(
+			status=status.HTTP_201_CREATED,
+			data={"detail": "group '{0}' added.".format(group.name), "data": serializer.data})
+
 
 class EditAndDeleteGroupView(APIView):
 	""" admin only with put 'name' and 'users' , users is an array of users key. and group key should include in url. """
@@ -65,7 +76,7 @@ class EditAndDeleteGroupView(APIView):
 			Attributes
 			----------
 			user -> django.contrib.auth.models.User(object) : authenticated user which sending the request
-			group -> api.models.Group(object) : contain group object which key is group_key 
+			group -> api.models.Group(object) : contain group object which key is group_key
 
 			Responses
 			----------
@@ -85,9 +96,15 @@ class EditAndDeleteGroupView(APIView):
 
 		user = request.user
 		if not is_admin_user(group_key, user):
-			return Response(status=status.HTTP_403_FORBIDDEN, data={"detail": "you dont have permission to perform this action."})
+			return Response(
+				status=status.HTTP_403_FORBIDDEN,
+				data={"detail": "you dont have permission to perform this action."})
+
 		if len(request.data) == 0:
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "no new data provided."})
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "no new data provided."})
+
 		group = get_object_or_404(Group, key=group_key)
 		if "name" in request.data:
 			group.name = request.data["name"]
@@ -102,13 +119,12 @@ class EditAndDeleteGroupView(APIView):
 				profile.group.remove(group)
 		return Response(status=status.HTTP_200_OK, data={"detail": "modified"})
 
-
 	def delete(self, request, group_key, format=None):
 		"""
 			Attributes
 			----------
 			user -> django.contrib.auth.models.User(object) : authenticated user which sending the request
-			group -> api.models.Group(object) : contain group object which key is group_key 
+			group -> api.models.Group(object) : contain group object which key is group_key
 
 			Responses
 			----------
@@ -124,10 +140,15 @@ class EditAndDeleteGroupView(APIView):
 
 		user = request.user
 		if not is_admin_user(group_key, user):
-			return Response(status=status.HTTP_403_FORBIDDEN, data={"detail": "you dont have permission to perform this action."})
+			return Response(
+				status=status.HTTP_403_FORBIDDEN,
+				data={"detail": "you dont have permission to perform this action."})
+
 		group = get_object_or_404(Group, key=group_key)
 		group.delete()
-		return Response(status=status.HTTP_200_OK, data={"detail": "group '{0}' deleted.".format(group.name)})
+		return Response(
+			status=status.HTTP_200_OK,
+			data={"detail": "group '{0}' deleted.".format(group.name)})
 
 
 class CreateAndGetMovieView(APIView):
@@ -170,11 +191,19 @@ class CreateAndGetMovieView(APIView):
 		user = request.user
 		user_movies_count = Movie.objects.filter(user=user).count()
 		if user_movies_count >= MOVIE_PER_USER:
-			return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data={"detail": "you reached the limit of adding movie. limit:{0}".format(MOVIE_PER_USER)})
-		if not "name" in request.data:
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "'name' is required."})
+			return Response(
+				status=status.HTTP_406_NOT_ACCEPTABLE,
+				data={"detail": "you reached the limit of adding movie. limit:{0}".format(MOVIE_PER_USER)})
+
+		if "name" not in request.data:
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST, data={"detail": "'name' is required."})
+
 		if Movie.objects.filter(user=user, name=request.data["name"]).exists():
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "movie with this name already exists."})
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "movie with this name already exists."})
+
 		movie = Movie.objects.create(name=request.data["name"], user=user)
 		if "description" in request.data:
 			movie.description = request.data["description"]
@@ -190,7 +219,9 @@ class CreateAndGetMovieView(APIView):
 			movie.review = request.data["review"]
 
 		movie.save()
-		return Response(status=status.HTTP_201_CREATED, data={"detail": "movie '{0}' created".format(movie.name)})
+		return Response(
+			status=status.HTTP_201_CREATED,
+			data={"detail": "movie '{0}' created".format(movie.name)})
 
 	def get(self, request, format=None):
 		"""
@@ -250,9 +281,15 @@ class EditAndDeleteMovieView(APIView):
 		user = request.user
 		movie = get_object_or_404(Movie, key=key)
 		if movie.user != user:
-			return Response(status=status.HTTP_401_UNAUTHORIZED, data={"detail": "you dont have permission for this movie."})
+			return Response(
+				status=status.HTTP_401_UNAUTHORIZED,
+				data={"detail": "you dont have permission for this movie."})
+
 		if len(request.data) == 0:
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "no new data provided."})
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "no new data provided."})
+
 		if "name" in request.data:
 			movie.name = request.data["name"]
 		if "description" in request.data:
@@ -271,14 +308,15 @@ class EditAndDeleteMovieView(APIView):
 			movie.review = request.data["review"]
 		movie.save()
 		return Response(status=status.HTTP_200_OK, data={"detail": "updated"})
-		
 
 	def delete(self, request, key, format=None):
 		"""
 			Attributes
 			----------
-			user -> django.contrib.auth.models.User(object) : authenticated user which sending the request
-			movie -> api.models.Movie(object) : contain movie object that key passed to url
+			user -> django.contrib.auth.models.User(object) : authenticated user
+			 which sending the request
+			movie -> api.models.Movie(object) : contain movie object that key passed to
+			 url
 
 			Responses
 			----------
@@ -294,10 +332,14 @@ class EditAndDeleteMovieView(APIView):
 		user = request.user
 		movie = get_object_or_404(Movie, key=key)
 		if movie.user != user:
-			return Response(status=status.HTTP_403_FORBIDDEN, data={"detail": "you dont have permission for this movie."})
-		movie.delete()
-		return Response(status=status.HTTP_200_OK, data={"detail": "movie '{0}' deleted.".format(movie.name)})
+			return Response(
+				status=status.HTTP_403_FORBIDDEN,
+				data={"detail": "you dont have permission for this movie."})
 
+		movie.delete()
+		return Response(
+			status=status.HTTP_200_OK,
+			data={"detail": "movie '{0}' deleted.".format(movie.name)})
 
 
 class GetRandomMovieView(APIView):
@@ -308,22 +350,37 @@ class GetRandomMovieView(APIView):
 		"""
 			Attributes
 			----------
+			user -> django.contrib.auth.models.User(object) : authenticated user which sending the request
+			all_group_movie_keys -> list : store value of all_movies_in_group function
+			selected_movie_key -> str : a random movie key choosed from all_group_movie_keys list
+			movie -> api.models.Movie(object) : contain Movie object , with selected_movie_key key
+			serializer -> api.serializers.MovieSerializer(object) : contain serialized data of 'movie'
+
 			Responses
 			----------
+			404 -> key="detail", value="Not found." : given group key object not found
+			401 -> key="detail", value="you dont have permission for this group." : if user not a member of group
+			404 -> key="detail", value="Not found." : if choosed movie as random corrupt or delete
+			200 -> return serialized data of the choosed movie
+
 			Input Types
 			----------
+			key -> String : in the url
 		"""
 
 		user = request.user
-		group = get_object_or_404(Group, key=key)
 
 		if not have_permission_for_group(key, user):
-			return Response(status=status.HTTP_401_UNAUTHORIZED, data={"detail": "you dont have permission for this group."})
+			return Response(
+				status=status.HTTP_401_UNAUTHORIZED,
+				data={"detail": "you dont have permission for this group."})
+
 		all_group_movie_keys = all_movies_in_group(key)
 		selected_movie_key = random.choice(all_group_movie_keys)
 		movie = get_object_or_404(Movie, key=selected_movie_key)
 		serializer = MovieSerializer(instance=movie)
 		return Response(status=status.HTTP_200_OK, data=serializer.data)
+
 
 class SubmitMovieView(APIView):
 	""" every user that is a group member can submit a movie. should pass group key and movie key in url. """
@@ -342,16 +399,25 @@ class SubmitMovieView(APIView):
 		user = request.user
 		group_obj = get_object_or_404(Group, key=group)
 		if not have_permission_for_group(group, user):
-			return Response(status=status.HTTP_401_UNAUTHORIZED, data={"detail": "you dont have permission for this group."})
+			return Response(
+				status=status.HTTP_401_UNAUTHORIZED,
+				data={"detail": "you dont have permission for this group."})
+
 		all_movies_keys = all_movies_in_group(group)
-		if not movie in all_movies_keys:
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "movie not found as one of the group members movie."})
+		if movie not in all_movies_keys:
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "movie not found as one of the group members movie."})
+
 		movie = get_object_or_404(Movie, key=movie)
 		group_obj.movie_of_the_week = movie
 		group_obj.save()
 		movie.watched = True
 		movie.save()
-		return Response(status=status.HTTP_200_OK, data={"detail": "'{0}' selected as movie of the week.".format(movie.name)})
+		return Response(
+			status=status.HTTP_200_OK,
+			data={"detail": "'{0}' selected as movie of the week.".format(movie.name)})
+
 
 class AllUserGroups(APIView):
 	""" return all groups of authenticated user. """
@@ -372,6 +438,7 @@ class AllUserGroups(APIView):
 		serializer = GroupSerializer(instance=groups, many=True)
 		return Response(status=status.HTTP_200_OK, data=serializer.data)
 
+
 class AllGroupMembersProfile(APIView):
 	""" return all group memebers of the group. group key should pass in url and available only for group members. """
 	permission_classes = (permissions.IsAuthenticated, )
@@ -388,7 +455,10 @@ class AllGroupMembersProfile(APIView):
 
 		user = request.user
 		if not have_permission_for_group(group_key, user):
-			return Response(status=status.HTTP_401_UNAUTHORIZED, data={"detail": "you dont have permission for this group."})
+			return Response(
+				status=status.HTTP_401_UNAUTHORIZED,
+				data={"detail": "you dont have permission for this group."})
+
 		group = get_object_or_404(Group, key=group_key)
 		all_members = Profile.objects.filter(group=group)
 		group_serializer = GroupMemberSerializer(instance=all_members, many=True)
@@ -411,11 +481,15 @@ class GenerateInviteCode(APIView):
 
 		user = request.user
 		if not is_admin_user(group_key, user):
-			return Response(status=status.HTTP_403_FORBIDDEN, data={"detail": "you dont have permission to perform this action."})
+			return Response(
+				status=status.HTTP_403_FORBIDDEN,
+				data={"detail": "you dont have permission to perform this action."})
+
 		group = get_object_or_404(Group, key=group_key)
 		group.invite_code = invite_code()
 		group.save()
 		return Response(status=status.HTTP_200_OK, data={"code": group.invite_code})
+
 
 class JoinGroup(APIView):
 	""" available for all authenticated users. invitation code should pass in url. """
@@ -433,10 +507,16 @@ class JoinGroup(APIView):
 
 		user = request.user
 		if not Group.objects.filter(invite_code=invite_code).exists():
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "requested group does not exist, please inform the admin to generate a new key."})
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "requested group does not exist, please inform the admin to generate a new key."})
+
 		group = get_object_or_404(Group, invite_code=invite_code)
 		user.profile.group.add(group)
-		return Response(status=status.HTTP_200_OK, data={"detail": "you are now a member of group '{0}'.".format(group.name)})
+		return Response(
+			status=status.HTTP_200_OK,
+			data={"detail": "you are now a member of group '{0}'.".format(group.name)})
+
 
 class LeaveGroup(APIView):
 	""" available for all authenticated users. group_key should pass in url. """
@@ -451,10 +531,15 @@ class LeaveGroup(APIView):
 			Input Types
 			----------
 		"""
-		
+
 		user = request.user
 		group = get_object_or_404(Group, key=group_key)
 		if not user.profile.group.filter(group).exists():
-			return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "you are not member of this group."})
+			return Response(
+				status=status.HTTP_400_BAD_REQUEST,
+				data={"detail": "you are not member of this group."})
+
 		user.profile.group.remove(group)
-		return Response(status=status.HTTP_200_OK, data={"detail": "you are not member of '{}' group anymore.".format(group.name)})
+		return Response(
+			status=status.HTTP_200_OK,
+			data={"detail": "you are not member of '{}' group anymore.".format(group.name)})
